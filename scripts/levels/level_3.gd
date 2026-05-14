44acb0ac829c61eec7b33d9014434b8cf5d5106e
Salmon228 <salmon228@inbox.ru>
2026-05-14 22:10:40 +0300

    Added First 3 levels

---
+++ scripts/levels/level_3.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+var player: CharacterBody2D
+
+func _ready() -> void:
+	player = $CharacterBody2D
---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 25000.0
+
+func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction_y:
+		velocity.y = direction_y * SPEED * delta
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
