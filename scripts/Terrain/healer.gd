7aab22327e53d9f0ab7e5ee57bea2f16079c97d5
Salmon228 <salmon228@inbox.ru>
2026-05-23 20:14:35 +0300

    Started re-creating level 5
    Also fixed healer bug (mushroom arrows don`t break healers)

--- scripts/Terrain/healer.gd
+++ scripts/Terrain/healer.gd
@@ -4,7 +4,7 @@ extends StaticBody2D
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var body = area.get_parent()
 	
-	if "mushroom" in body.name.to_lower() or "player" in body.name.to_lower() and not "mushroom_arrow" in body.name.to_lower():
+	if ("mushroom" in body.name.to_lower() or "player" in body.name.to_lower()) and body.get_node_or_null("I_AM_MUSHROOM_ARROW") == null:
 		body.heal(50)
 		$Area2D.queue_free()
 		$Sprite2D.hide()
