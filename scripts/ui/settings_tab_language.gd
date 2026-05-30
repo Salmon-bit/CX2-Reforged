86f0621739dbf3880215122b746c22b5c338fb4a
Salmon228 <salmon228@inbox.ru>
2026-05-30 19:19:32 +0300

    Added more translations

--- scripts/ui/settings_tab_language.gd
+++ scripts/ui/settings_tab_language.gd
@@ -14,3 +14,19 @@ func _on_eng_pressed() -> void:
 	Autoload.save_data()
 	TranslationServer.set_locale("en")
 	get_tree().current_scene.update_textures()
+
+
+func _on_de_pressed() -> void:
+	Autoload.click()
+	Autoload.data.lang = "de"
+	Autoload.save_data()
+	TranslationServer.set_locale("de")
+	get_tree().current_scene.update_textures()
+
+
+func _on_fr_pressed() -> void:
+	Autoload.click()
+	Autoload.data.lang = "fr"
+	Autoload.save_data()
+	TranslationServer.set_locale("fr")
+	get_tree().current_scene.update_textures()
