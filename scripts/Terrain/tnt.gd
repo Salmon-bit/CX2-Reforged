a96c88da1eef2e43c8ccd8b293ec3b2f4a029561
Salmon228 <salmon228@inbox.ru>
2026-07-22 19:18:43 +0300

    Added tnt chain explode

--- scripts/Terrain/tnt.gd
+++ scripts/Terrain/tnt.gd
@@ -1,29 +1,48 @@
 extends StaticBody2D
 
 var targets = []
+var is_exploding = false
+var tnts = []
 
 func explode():
-	$ExpoldeSprite.show()
-	$Timer.start()
-	for target in targets:
-		target.attack(40)
+	if not is_exploding:
+		is_exploding = true
+		$ExpoldeSprite.show()
+		$Timer.start()
+		for target: Node2D in targets:
+			if target.get_node_or_null("I_AM_TNT"):
+				tnts.append(target) # Delayed explode
+			else:
+				target.attack(40)
 
 
 func _on_explode_area_body_entered(body: Node2D) -> void:
-	if not "arrow" in body.name.to_lower() and body is not StaticBody2D:
-		targets.append(body)
+	if\
+	(body.get_node_or_null("I_AM_MUSHROOM_ARROW") == null and body.get_node_or_null("I_AM_ARROW") == null)\
+	and\
+	(body is not StaticBody2D or body.get_node_or_null("I_AM_TNT") != null):
+			targets.append(body)
 
 
 func _on_explode_area_body_exited(body: Node2D) -> void:
-	if not "arrow" in body.name.to_lower() and body is not StaticBody2D:
+	if\
+	(body.get_node_or_null("I_AM_MUSHROOM_ARROW") == null and body.get_node_or_null("I_AM_ARROW") == null)\
+	and\
+	(body is not StaticBody2D or body.get_node_or_null("I_AM_TNT") != null):
 		targets.pop_at(targets.find(body))
 
 
 func _on_timer_timeout() -> void:
+	for i in tnts:
+		if i != null:
+			i.explode()
 	queue_free()
 
+func attack(_i: int):
+	explode()
 
 func _on_detonate_hitbox_area_entered(area: Area2D) -> void:
 	print("Let me detonate " + area.get_parent().name)
 	if "arrow" in area.get_parent().name.to_lower():
+		is_exploding = false
 		explode()
